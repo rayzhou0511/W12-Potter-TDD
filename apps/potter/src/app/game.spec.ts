7a1dd2189e151863f41f820d1378b095c73a999e
rayzhou0511 <rayzhou05@gmail.com>
2022-05-09 13:16:54 +0800

    testSimpleDiscounts

--- apps/potter/src/app/game.spec.ts
+++ apps/potter/src/app/game.spec.ts
@@ -8,14 +8,16 @@ describe('Game', () => {
     game = new Game();
   });
 
+  /*
   test('testbasics', () => {
     game.buy([2,0,4,1,0]);
     expect(game.price).toBe(56);
   });
-  /*
-  test('testSimpleDiscounts', () => {
-    game.buy([1,0,1,1,0]);
-    expect(game.price).toBe(8*3*0.9);
-  }); 
   */
+
+  test('testSimpleDiscounts', () => {
+    game.buy([1,1,1,1,1]);
+    expect(game.price).toBe(8*5*0.75);
+  });
+
 });
--- apps/potter/src/app/game.ts
+++ apps/potter/src/app/game.ts
@@ -1,6 +1,6 @@
 export class Game {
     private basket: number[] = []; 
-
+    private book_price = 8;
     buy(episode: number[]){
         for(let i = 0; i < 5; i++){
             this.basket[i] = episode[i];
@@ -9,10 +9,33 @@ export class Game {
 
     get price(){
         let score = 0;
+        let bookcount = 0;
         for(let epi = 0; epi < 5; epi++){
-            score += this.basket[epi] * 8
+            bookcount += this.basket[epi];
         }
+        score = bookcount * this.WhichDiscount(bookcount) * this.book_price;
         return score;
+    }
+
+    private WhichDiscount(bookcount: number){
+        if(bookcount === 1){
+            return 1;
+        }
+        else if(bookcount === 2){
+            return 0.95;
+        }
+        else if(bookcount === 3){
+            return 0.9;
+        }
+        else if(bookcount === 4){
+            return 0.8;
+        }
+        else if(bookcount === 5){
+            return 0.75;
+        }
+        else{
+            return 0;
+        }
     }
 
 }
