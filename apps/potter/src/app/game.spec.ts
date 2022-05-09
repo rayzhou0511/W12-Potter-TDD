a4a426dbfada91fbcd147b7be8bd56d5da8f60f1
rayzhou0511 <rayzhou05@gmail.com>
2022-05-09 12:39:25 +0800

    testbasic

--- apps/potter/src/app/game.spec.ts
+++ apps/potter/src/app/game.spec.ts
@@ -1,7 +1,16 @@
 import { Game } from './game';
 
 describe('Game', () => {
-  it('should create an instance', () => {
-    expect(new Game()).toBeTruthy();
+  let game : Game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
+  test('testbasic', () => {
+    game.buy(0,2);
+    game.buy(1,4);
+    game.buy(4,1)
+    expect(game.price).toBe(56);
   });
 });
--- apps/potter/src/app/game.ts
+++ apps/potter/src/app/game.ts
@@ -1,2 +1,16 @@
 export class Game {
+    private books: number[] = [0,0,0,0,0]; 
+
+    buy(episode: number, num: number){
+        this.books[episode] = num;
+    }
+
+    get price(){
+        let score = 0;
+        for(let epi = 0; epi < 5; epi++){
+            score += this.books[epi] * 8
+        }
+        return score;
+    }
+
 }
